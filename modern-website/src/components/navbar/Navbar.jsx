2c9f13b61ae2a0f41ce43df5d4b84ba784de1296
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-10 18:32:45 +0330

    up

--- modern-website/src/components/navbar/Navbar.jsx
+++ modern-website/src/components/navbar/Navbar.jsx
@@ -10,6 +10,13 @@ function Navbar() {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">what is GPT?</a></p>
+          <p><a href="#possibility">open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
     </div>
   )
