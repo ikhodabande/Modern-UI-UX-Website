b06bb847ee929592633728f58a85cd4aed309d35
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-10 18:52:30 +0330

    up

--- modern-website/src/components/navbar/Navbar.jsx
+++ modern-website/src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
-function Navbar() {
+const Navbar=()=> {
+  const [toggleMenu , setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -17,6 +18,14 @@ function Navbar() {
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>sign in</p>
+        <button type='button'>sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        
+
       </div>
     </div>
   )
