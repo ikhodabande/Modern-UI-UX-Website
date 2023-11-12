bfd586f967e06447663e55d1c885bf4ad80c2f6a
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-12 11:50:05 +0330

    added imgs from assests

--- modern-website/src/containers/header/Header.jsx
+++ modern-website/src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 function Header() {
   return (
@@ -8,6 +10,16 @@ function Header() {
         <h1 className="gradiant__text">
           Let's Build somthing amazing with GPT-3 OpenAI 
         </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptate suscipit hic repudiandae! Reprehenderit cum laborum aliquam fugit incidunt fuga?</p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder='your email address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+        </div>
       </div>
     </div>
   )
